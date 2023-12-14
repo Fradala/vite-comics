cc358dfbde0d89195beab86aa272bb100e046272
Francesco.dalascio <francesco.dalascio.2002@gmail.com>
2023-12-14 18:26:30 +0100

    metà info footer

--- src/components/FooterDc.vue
+++ src/components/FooterDc.vue
@@ -1,13 +1,21 @@
 <template lang="">
-    <div>
-        
-    </div>
+    <footer>
+        <infoFooter/>     
+    </footer>
 </template>
 <script>
+import infoFooter from './infoFooter.vue';
 export default {
     name: 'FooterDc',
+    components:{
+        infoFooter
+    }
 }
 </script>
 <style lang="scss" scoped>
+@use '../styles/partial/variables' as *;
+@use '../styles/partial/mixins' as *;
+
+
     
 </style>
---
+++ src/components/infoFooter.vue
@@ -0,0 +1,145 @@
+<template lang="">
+    <section class="info">
+        <div class="info-footer">
+
+        
+            <h2>
+                dc comics
+            </h2>
+            <nav>
+                <ul>
+                    <li>
+                        cose
+                    </li>
+                    <li>
+                        cose
+                    </li>
+                    <li>
+                        cose
+                    </li>
+                    <li>
+                        cose
+                    </li>
+                    <li>
+                        cose
+                    </li>
+                    <li>
+                        cose
+                    </li>
+                </ul>
+            </nav>
+            <h2>
+                dc 
+            </h2>
+            <nav>
+                <ul>
+                    <li>
+                        cose
+                    </li>
+                    <li>
+                        cose
+                    </li>
+                    <li>
+                        cose
+                    </li>
+                    <li>
+                        cose
+                    </li>
+                    <li>
+                        cose
+                    </li>
+                    <li>
+                        cose
+                    </li>
+                    <li>
+                        cose
+                    </li>
+                    <li>
+                        cose
+                    </li>
+                    <li>
+                        cose
+                    </li>
+                    <li>
+                        cose
+                    </li>
+                    <li>
+                        cose
+                    </li>
+                    <li>
+                        cose
+                    </li>
+                </ul>
+            </nav>
+            <h2>
+                sities
+            </h2>
+            <nav>
+                <ul>
+                    <li>
+                        cose
+                    </li>
+                    <li>
+                        cose
+                    </li>
+                    <li>
+                        cose
+                    </li>
+                    <li>
+                        cose
+                    </li>
+                    <li>
+                        cose
+                    </li>
+                    <li>
+                        cose
+                    </li>
+                </ul>
+            </nav>
+            <h2>
+                shop
+            </h2>
+            <nav>
+                <ul>
+                    <li>
+                        cose
+                    </li>
+                    <li>
+                        cose
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </section>
+    
+</template>
+<script>
+export default {
+    name: 'infoFooter',
+
+}
+
+</script>
+<style lang="scss" scoped>
+@use '../styles/partial/variables' as *;
+@use '../styles/partial/mixins' as *;
+
+
+section.info{
+
+    background-image: url(../assets/img/footer-bg.jpg);
+    width: 100%;
+    height: 400px;
+    
+
+    div.info-footer{
+        margin-left: 110px;
+        color: white;
+
+        ul{
+            list-style-type: none;
+        }
+    }
+}
+    
+</style>
