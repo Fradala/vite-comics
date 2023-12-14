f03f7feb83d2f240d99f0fd3a8b03c9fa1c058ab
Francesco.dalascio <francesco.dalascio.2002@gmail.com>
2023-12-14 18:01:57 +0100

    inserito main e main content

---
+++ src/components/ContenutoMainDc.vue
@@ -0,0 +1,28 @@
+<template lang="">
+    <section class="content">
+        <h1>
+            -- Contente goes hear 
+        </h1>
+    </section>
+</template>
+<script>
+export default {
+    name: 'ContenutoMainDc',
+    data(){
+        return {
+            listaDc: []
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+@use '../styles/partial/variables' as *;
+@use '../styles/partial/mixins' as *;
+    
+
+section.content{
+        margin-left: 100px;
+        color: white;
+        @include flex(row, start, start)
+    }
+</style>
--- src/components/MainDc.vue
+++ src/components/MainDc.vue
@@ -1,13 +1,25 @@
 <template lang="">
-    <div>
-        
-    </div>
+    <main>
+        <ContenutoMainDc/>
+    </main>
+   
 </template>
 <script>
+import ContenutoMainDc from './ContenutoMainDc.vue';
 export default {
     name: 'MainDc',
+    components:{
+        ContenutoMainDc
+    }
 }
 </script>
 <style lang="scss" scoped>
+@use '../styles/partial/variables' as *;
+@use '../styles/partial/mixins' as *;
+
+main{
+    background-color: black;
+    padding: 2rem;
+}
     
 </style>
