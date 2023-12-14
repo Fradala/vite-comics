bc46b879c32187bb19a55be9947092578bad5001
Francesco.dalascio <francesco.dalascio.2002@gmail.com>
2023-12-14 15:21:06 +0100

    inizio

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template lang="">
+  <div>
+    
+  </div>
+</template>
+<script>
+import HeaderCd from './components/HeaderCd.vue';
+
+export default {
+  components:{
+    HeaderCd
+  }
+  
+}
+</script>
+<style lang="©">
+  
+</style>
