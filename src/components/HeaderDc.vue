c1cf228c4b290189293b7531f3497be6aefb9260
Francesco.dalascio <francesco.dalascio.2002@gmail.com>
2023-12-14 16:56:12 +0100

    problema con lo style

--- src/components/HeaderDc.vue
+++ src/components/HeaderDc.vue
@@ -1,8 +1,43 @@
-<template>
-    <div>
-       
-        
-    </div>
+<template lang="">
+   <header>
+    <section class="logo">
+        <img src="../assets/img/dc-logo.png" alt="logo dc">
+    </section>
+    <nav class="navigation">
+        <ul>
+            <li>
+                character
+            </li>
+            <li>
+                comix
+            </li>
+            <li>
+                movie
+            </li>
+            <li>
+                tv
+            </li>
+            <li>
+                game
+            </li>
+            <li>
+                collectible
+            </li>
+            <li>
+                videos
+            </li>
+            <li>
+                fans
+            </li>
+            <li>
+                news
+            </li>
+            <li>
+                shop
+            </li>
+        </ul>
+    </nav>
+   </header>
 </template>
 <script>
 export default {
@@ -11,5 +46,18 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+@use '../styles/partial/variables' as *;
+@use '../styles/partial/mixins' as *;
+
+
+header{
+    @include flex(row, space-between, center)
+
+   .navigation 
+
+    
+}
+
+
     
 </style>
